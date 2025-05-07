5c23022bc77ace372ccb86bf6e3d9afd5a50eb29
양선미 <czaeopd.gmail.com>
2025-05-07 20:11:31 +0900

    추가

--- tt.py
+++ tt.py
@@ -3,3 +3,11 @@ import streamlit as st
 st.title('title')
 st.header('this is header')
 st.subheader('this is subheader')
+
+co1, co2 = st.columns([2, 3])
+with co1 :
+    st.title("here is  column1 title")
+
+with co2:
+    st.title("here is column2 title")
+    st.checkbox("this is checkbox1 in co2")
