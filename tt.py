c7c069759d6d89aac4e369035312e43241fe0db5
양선미 <czaeopd.gmail.com>
2025-05-07 20:18:41 +0900

    추가

--- tt.py
+++ tt.py
@@ -5,8 +5,14 @@ st.header('this is header')
 st.subheader('this is subheader')
 
 co1, co2 = st.columns([2, 3])
+tab1, tab2 = st.tabs(['Tab A', 'Tab B'])
+
 with co1 :
     st.title("here is  column1 title")
+    with tab1:
+        st.write("hello")
+    with tab2:
+        st.write("hi")
 
 with co2:
     st.title("here is column2 title")
