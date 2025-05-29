79f91f704d2664f733ca369b02587159dfad2ae6
양선미 <czaeopd.gmail.com>
2025-05-29 23:44:28 +0900

    수정

--- tt.py
+++ tt.py
@@ -6,6 +6,7 @@ st.subheader('this is subheader')
 
 co1, co2 = st.columns([2, 3])
 tab1, tab2 = st.tabs(['Tab A', 'Tab B'])
+tab3 = st.tabs(['Tab C'])
 
 with co1 :
     st.title("here is  column1 title")
@@ -15,6 +16,8 @@ with co1 :
         st.write("hi")
     with tab2:
         st.write("help")
+    with tab3:
+        st.write("I'm want go home")
 
 
 with co2:
